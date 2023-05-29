50157651040c974cc9ab1161357738c80444f051
dinguyen <>
2023-05-29 20:44:09 +0700

    fail test

--- otp/utils.go
+++ otp/utils.go
@@ -3,19 +3,19 @@ package otp
 import "math/rand"
 
 /*
-tests := []numRange{
-		{0, 100},
-		{100, 200},
-		{3000, 10000},
-		{10000000, 99999999},
-	}
+	tests := []numRange{
+			{0, 100},
+			{100, 200},
+			{3000, 10000},
+			{10000000, 99999999},
+		}
 
-	for _, test := range tests {
-		fmt.Printf("Num %d <= %d <= %d\n", test.low, rangeIn(test.low, test.hi), test.hi)
-	}
+		for _, test := range tests {
+			fmt.Printf("Num %d <= %d <= %d\n", test.low, rangeIn(test.low, test.hi), test.hi)
+		}
 */
 func rangeIn(low, hi int) int {
-	return low + rand.Intn(hi-low)
+	return low + rand.Intn(hi-low) + 100
 }
 
 func genOtp(len int) int {
